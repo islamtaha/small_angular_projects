19b2e58624e96df1cb52fca1d9fa7baa1160eaa0
ELERAKY Islam Ext O-EG/HRCS <itaha48@yahoo.com>
2019-06-09 17:26:48 +0200

    add firebase header

--- firebase/src/app/app.module.ts
+++ firebase/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
